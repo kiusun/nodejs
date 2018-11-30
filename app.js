66a9dfbb85337f79671e21e6d392e254972009fa
kiusun <kiusunbn@gmail.com>
2018-11-30 13:59:35 +0700

    add ip2location

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var app = express();
 var path = require('path'); 
-var maxmind = require('maxmind');
-var node-geolite2 = require('node-geolite2');
-node-geolite2.init();
+const Reader = require('@maxmind/geoip2-node').Reader;
+var node_geolite2 = require('node-geolite2');
+node_geolite2.init();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs");
 app.set("views" , __dirname + "/views");
@@ -11,8 +11,16 @@ app.get("/addfriend", function(req , res){
     res.render("addfriend" , {title : "Add friends"});
 });
 app.get("/getuid", function(req , res){
-    var location = node-geolite2.getGeoDataSync("118.112.56.251");
-    console.log(location);
+    Reader.open('/usr/local/share/GeoIP/GeoIP2-ISP.mmdb').then(reader => {
+        const response = reader.isp('128.101.101.101');
+      
+        console.log(response.autonomousSystemNumber); // 217
+        console.log(response.autonomousSystemOrganization); // 'University of Minnesota'
+        console.log(response.isp); // 'University of Minnesota'
+        console.log(response.organization); // 'University of Minnesota'
+      
+        console.log(response.ipAddress); // '128.101.101.101'
+      });
     res.render("getuid" , {title : "Get UID"});
     
     
